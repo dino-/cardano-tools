192df93732c7488096da7b81c8285b04dbb71710
Dino Morelli <dino@ui3.info>
2021-09-06 10:22:08 -0400

    Made code fixes based on shellcheck recommendations

--- query-tip.sh
+++ query-tip.sh
@@ -3,4 +3,4 @@
 set -e
 
 ${CARDANO_CLI:-cardano-cli} query tip \
-  --testnet-magic ${TESTNET_MAGIC_NUM:?ERROR: TESTNET_MAGIC_NUM not set}
+  --testnet-magic "${TESTNET_MAGIC_NUM:?ERROR: TESTNET_MAGIC_NUM not set}"
--- query-utxo.sh
+++ query-utxo.sh
@@ -3,5 +3,5 @@
 set -e
 
 ${CARDANO_CLI:-cardano-cli} query utxo \
-  --testnet-magic ${TESTNET_MAGIC_NUM:?ERROR: TESTNET_MAGIC_NUM not set} \
-  --address ${1:?Usage: $0 ADDRESS}
+  --testnet-magic "${TESTNET_MAGIC_NUM:?ERROR: TESTNET_MAGIC_NUM not set}" \
+  --address "${1:?Usage: $0 ADDRESS}"
